a158de8eba160ed6189827c502981997004cc77c
Nahyeon Kim <nah0722@naver.com>
2016-04-11 21:01:29 +0900

    #8 add Splash Activity

---
+++ app/src/main/java/kr/hs/emirim/nahyeonkim/monchicken/SplashActivity.java
@@ -0,0 +1,27 @@
+package kr.hs.emirim.nahyeonkim.monchicken;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
